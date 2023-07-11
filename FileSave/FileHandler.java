40373a937e758ae4ab6c680bb34fedf7b27ab626
Dimash <mwasikzano@gmail.com>
2023-07-11 22:19:22 +0600

    HW_for_seminar_4

--- FileSave/FileHandler.java
+++ FileSave/FileHandler.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 public class FileHandler{
-    public static void saveFamilyTree(FamilyTree familyTree, String fileName) {
+    public  void saveFamilyTree(FamilyTree familyTree, String fileName) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
             oos.writeObject(familyTree);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static FamilyTree loadFamilyTree(String fileName) {
+    public FamilyTree loadFamilyTree(String fileName) {
         FamilyTree familyTree = null;
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
             familyTree = (FamilyTree) ois.readObject();
